c85674ee5ac013bb585fa0babe46342b041a9fb2
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-11-04 19:58:03 +0100

    essai deploiement 1

--- pocketbase/pb.ts
+++ pocketbase/pb.ts
@@ -1,5 +1,8 @@
 import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
-
-const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    //localhost = machine de dev
+else path = 'http://localhost:8030'   //localhost = machine de déploiement
+const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
