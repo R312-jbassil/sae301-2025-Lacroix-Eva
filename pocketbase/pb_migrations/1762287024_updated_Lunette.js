9d31f724616f9584de4c80a1e88d31ab7bf7a83a
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-11-04 21:23:47 +0100

    essai deploiement

---
+++ pocketbase/pb_migrations/1762287024_updated_Lunette.js
@@ -0,0 +1,45 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3690071135")
+
+  // remove field
+  collection.fields.removeById("text3949269562")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "convertURLs": false,
+    "hidden": false,
+    "id": "editor3949269562",
+    "maxSize": 0,
+    "name": "code_svg",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "editor"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3690071135")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3949269562",
+    "max": 0,
+    "min": 0,
+    "name": "code_svg",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // remove field
+  collection.fields.removeById("editor3949269562")
+
+  return app.save(collection)
+})
