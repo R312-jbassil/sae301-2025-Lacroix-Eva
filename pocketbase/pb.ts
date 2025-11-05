3d56662803d2b8a99d3ba9e29e50b8fbcd7802f3
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-11-05 13:56:21 +0100

    test

--- pocketbase/pb.ts
+++ pocketbase/pb.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 if(import.meta.env.MODE === 'development')
-    path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'https://sae301.eva-lacroix.fr:443'   //localhost = machine de déploiement
+    path = 'https://sae301.eva-lacroix.fr:443'    //localhost = machine de dev
+else path = 'http://localhost:8020'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
