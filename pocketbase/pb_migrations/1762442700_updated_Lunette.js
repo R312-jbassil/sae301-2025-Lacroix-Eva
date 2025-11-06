51364947bb28dda9bcf0d0fab69a742dcff8f330
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-11-06 17:16:04 +0100

    configurateur

---
+++ pocketbase/pb_migrations/1762442700_updated_Lunette.js
@@ -0,0 +1,29 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3690071135")
+
+  // remove field
+  collection.fields.removeById("text1807006754")
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3690071135")
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1807006754",
+    "max": 0,
+    "min": 0,
+    "name": "chat_history2",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
